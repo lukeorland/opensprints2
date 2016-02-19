f3193936be3a5aff53315c76bb472f1aeac63f19
Luke Orland <orluke@gmail.com>
2016-02-19 00:10:58 -0500

    Simple PubSub

--- main.js
+++ main.js
@@ -10,8 +10,6 @@ const BrowserWindow = electron.BrowserWindow; // Module to create native browser
 var nightlife = require('nightlife-rabbit');
 var autobahn = require('autobahn');
 var http = require('http');
-var fs = require('fs');
-
 
 function wampStuff() {
   // Create a secure webserver as transport for the WebSocket connections.
@@ -24,75 +22,42 @@ function wampStuff() {
   // if the requested realm doesn't exists.
   var router = nightlife.createRouter({
     httpServer: transport,
-    port: 8080,
-    path: '/secure-nightlife',
+    port: 9000,
+    path: '/ws',
     autoCreateRealms: false
   });
 
   // Create an example realm.
-  router.createRealm('com.example.inge');
+  router.createRealm('realm1');
 
-  // Create an example service which return a random integer when called.
-  // This service maybe runs on a different machine or in the browser.
-  var serviceA = new autobahn.Connection({
-    url: 'ws://localhost:8080/secure-nightlife',
-    realm: 'com.example.inge'
+  var connection = new autobahn.Connection({
+    url: 'ws://127.0.0.1:9000/ws',
+    realm: 'realm1'
   });
 
-  serviceA.onopen = function(session) {
-    session.register('com.example.random', function(args, kwargs, details) {
-        return Math.floor(Math.random() * Math.pow(2, 53));
-      })
-      .then(function(registration) {
-        console.log('service with id %d registered.', registration.id);
-      })
-      .catch(function(err) {
-        console.log('cannot register service!');
-        process.exit(2);
-      })
-      .done();
+  connection.onopen = function(session) {
+
+    // publish an event
+    var counter = 0;
+    setInterval(function() {
+      session.publish('com.opensprints.hello', [counter], {}, {
+          acknowledge: true
+        })
+        .then(
+
+          function(publication) {
+            //console.log("published, publication ID is ", publication);
+          },
+
+          function(error) {
+            console.log("publication error", error);
+          }
+        );
+      counter += 1;
+    }, 200);
   };
 
-  serviceA.onclose = function(reason) {
-    console.log('service closed', reason);
-    process.exit(2);
-  };
-
-  setTimeout(function() {
-    serviceA.open();
-  }, 500);
-
-  // Create an example service which will populate the
-  // current datetime every two secundes.
-  // Clients can subscribe to this topic in the same manner.
-  var serviceB = new autobahn.Connection({
-    url: 'ws://localhost:8080/secure-nightlife',
-    realm: 'com.example.inge'
-  });
-
-  serviceB.onopen = function(session) {
-    session.subscribe('com.example.time', function() {})
-      .then(function(subscription) {
-        console.log('subscribed to topic.', subscription.id);
-
-        setInterval(function() {
-          session.publish('com.example.time', [new Date().getTime()]);
-        }, 1927);
-      })
-      .catch(function(err) {
-        console.log('cannot subscribe to topic!', err);
-      })
-      .done();
-  };
-
-  serviceB.onclose = function(reason) {
-    console.log('service closed', reason);
-    process.exit(2);
-  };
-
-  setTimeout(function() {
-    serviceB.open();
-  }, 500);
+  connection.open();
 }
 
 // Keep a global reference of the window object, if you don't, the window will
