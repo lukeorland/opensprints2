c6e3cff183e2e085fce13f5a2e8eb5f04c757e3d
Luke Orland <orluke@gmail.com>
2016-07-21 23:09:58 -0400

    No idea

---
+++ main.jsx
@@ -0,0 +1,14 @@
+var HubStatusContainer = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <h3>hello1</h3>
+            </div>
+        )
+    }
+});
+
+ReactDOM.render(
+    <HubStatusContainer /> ,
+    document.getElementById('app')
+);
